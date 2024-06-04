bf52097a16c0fcd97e397f3b88ac961455918667
MrExplode <me@sunstorm.rocks>
2024-06-04 13:32:15 +0200

    feat: logging config

---
+++ src/main/java/me/sunstorm/weather/Bootstrap.java
@@ -0,0 +1,11 @@
+package me.sunstorm.weather;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        log.info("Starting Glance weather extension");
+    }
+}
